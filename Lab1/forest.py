d9e9ad9064559c469cc022b357e0d2547415398a
Ivan Gromakovskii <gie1994@gmail.com>
2016-11-15 17:36:07 +0300

    Rework OOB error calculation

--- Lab1/forest.py
+++ Lab1/forest.py
@@ -1,6 +1,6 @@
 from functools import partial
 from math import sqrt
-from operator import itemgetter
+from operator import attrgetter
 import random
 from typing import List, Iterable
 
@@ -22,10 +22,24 @@ class DecisionTreeMeta:
         return self._item_indices
 
 
+class ExtendedDecisionTree:
+
+    def __init__(self, tree: DecisionTree, meta: DecisionTreeMeta):
+        self._tree = tree
+        self._meta = meta
+
+    @property
+    def tree(self):
+        return self._tree
+
+    @property
+    def meta(self):
+        return self._meta
+
+
 class RandomForest(Classifier):
 
     def __init__(self, train_data: DataSet, quality_function: QualityF, trees_num: int):
-        self._train_data = train_data
         self._trees = []
         for _ in range(trees_num):
             f_num = train_data.features_num
@@ -34,31 +48,32 @@ class RandomForest(Classifier):
             sub_ds = DataSet(items)
             tree = build_decision_tree(sub_ds, quality_function, features_to_use)
             meta = DecisionTreeMeta(training_data=sub_ds, item_indices=item_indices, used_features=features_to_use)
-            self._trees.append((tree, meta))
+            self._trees.append(ExtendedDecisionTree(tree, meta))
 
     def classify(self, features: List[Feature]) -> Label:
-        return self._classify(map(itemgetter(0), self._trees), features)
+        return self._classify(map(attrgetter('tree'), self._trees), features)
 
-    def oob_error(self) -> List[float]:
+    def oob_error(self, train_data: DataSet) -> List[float]:
         res = []
-        for tree, tree_meta in self._trees:
-            items_num = len(self._train_data)
+        items_num = len(train_data)
+        for extended_tree in self._trees:
             total_classifications = 0
             total_errors = 0
             for i in range(items_num):
-                if i not in tree_meta.item_indices:
+                if i in extended_tree.meta.item_indices:
                     continue
 
-                item = self._train_data.items[i]
+                item = train_data.items[i]
                 total_classifications += 1
-                total_errors += int(item.label != tree.classify(item.features))
+                total_errors += int(item.label != extended_tree.tree.classify(item.features))
 
             print(total_classifications, total_errors)
             res.append(float(total_errors) / total_classifications)
 
         return res
 
-    def _classify(self, trees: Iterable[DecisionTree], features: List[Feature]):
+    @staticmethod
+    def _classify(trees: Iterable[DecisionTree], features: List[Feature]):
         s = sum(map(partial(DecisionTree.classify, features=features), trees))
         return -1 if s < 0 else 1
 
