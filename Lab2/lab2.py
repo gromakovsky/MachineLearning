52d2414a079e0ed27f3e9ff2e2911870ce2e8b1f
Ivan Gromakovskii <gie1994@gmail.com>
2016-11-23 00:00:21 +0300

    Implement PCA

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -1,4 +1,6 @@
-from ml.pca import pca
+import numpy as np
+
+from ml.pca import pca_eigens, broken_stick, transformation_matrix
 
 
 FILES = [
@@ -14,7 +16,16 @@ def main():
         with open(file) as f:
             data = [[float(i) for i in s.split()] for s in f.readlines()]
 
-        pca(data)
+        arr = np.array(data)
+        eigens = pca_eigens(arr)
+        k = broken_stick(eigens)
+        print('Number of principal components:', k)
+        m = transformation_matrix([e[1] for e in eigens], k)
+        transformed = (m @ arr.T).T
+        with open(file + '-transformed', mode='w') as out:
+            for item in transformed:
+                out.write(" ".join([str(i.real) for i in item]))
+                out.write('\n')
 
 
 if __name__ == '__main__':
--- ml/pca.py
+++ ml/pca.py
@@ -1,7 +1,51 @@
-from typing import List
+from operator import itemgetter
+from typing import List, Tuple
 import numpy as np
 
 
-def pca(data: List[List[float]]):
-    arr = np.array(data)
-    assert arr.shape == (1000, 200)
+fst = itemgetter(0)
+
+
+# data is array of objects, were each object is array of features
+def pca_eigens(arr: np.ndarray) -> List[Tuple[float, np.ndarray]]:
+    items_num, dimensionality = arr.shape
+    transposed = arr.transpose()
+    assert transposed.shape == (dimensionality, items_num)
+    mean_values = np.array([[np.mean(i)] for i in transposed])
+    assert mean_values.shape == (dimensionality, 1)
+
+    scatter_matrix = np.zeros((dimensionality, dimensionality))
+    for i in range(items_num):
+        d = arr[i].reshape(dimensionality, 1) - mean_values
+        scatter_matrix += d @ d.T
+
+    assert scatter_matrix.shape == (dimensionality, dimensionality)
+
+    eigen_values, eigen_vectors = np.linalg.eig(scatter_matrix)
+    eigen_pairs = [(abs(eigen_values[i]), eigen_vectors[:, i]) for i in range(dimensionality)]
+    eigen_pairs.sort(key=fst, reverse=True)
+    return eigen_pairs
+
+
+def broken_stick(eigens: List[Tuple[float, np.ndarray]]) -> int:
+    s = sum(map(fst, eigens))
+    n = len(eigens)
+    normalized = [i / s for i in map(fst, eigens)]
+    res = 0
+    for i in range(len(normalized)):
+        l = sum(1 / (j + 1) for j in range(i, n)) / n
+        if normalized[i] < l:
+            break
+
+        res += 1
+
+    return res
+
+
+def transformation_matrix(eigens: List[np.ndarray], k: int) -> np.ndarray:
+    assert isinstance(eigens, list)
+    assert len(eigens) > k
+    assert k > 0
+    res = np.array(list(map(list, eigens[:k])))
+    assert res.shape == (k, len(eigens))
+    return res
